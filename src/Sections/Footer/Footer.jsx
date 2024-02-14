8b02da0b913b208a08969f86f8568fa663d15efc
dev-SazzadHossain <sazzadssd512@gmail.com>
2024-02-14 16:28:41 +0600

    add_border

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -14,162 +14,154 @@ import { Copyright } from "@mui/icons-material";
 import logo from "../../assets/no-image.jpg";
 
 export const Footer = () => {
-    return (
-        <div>
-            <Container
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: { xs: 4, sm: 8 },
-                    py: { xs: 8, sm: 10 },
-                    textAlign: { sm: "center", md: "left" },
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: { xs: "column", sm: "row" },
-                        width: "100%",
-                        justifyContent: "space-between",
-                    }}
+  return (
+    <div>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: { xs: 4, sm: 8 },
+          py: { xs: 8, sm: 10 },
+          textAlign: { sm: "center", md: "left" },
+          borderTop: "1px solid #8884",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", sm: "row" },
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 4,
+              minWidth: { xs: "100%", sm: "60%" },
+            }}
+          >
+            <Box sx={{ width: { xs: "100%", sm: "60%" } }}>
+              <Box sx={{ pb: "10px" }}>
+                <img
+                  style={{
+                    objectFit: "cover",
+                    borderRadius: "80px",
+                  }}
+                  width={70}
+                  src={logo}
+                  alt="logo of sitemark"
+                />
+              </Box>
+              <Typography variant="body2" fontWeight={600} gutterBottom>
+                Newsletter
+              </Typography>
+              <Typography variant="body2" color="text.secondary" mb={2}>
+                Subscribe to our newsletter for weekly updates and promotions.
+              </Typography>
+              <Stack direction="row" spacing={1} useFlexGap>
+                <TextField
+                  id="outlined-basic"
+                  hiddenLabel
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  aria-label="Enter your email address"
+                  placeholder="Your email address"
+                  inputProps={{
+                    autocomplete: "off",
+                    ariaLabel: "Enter your email address",
+                  }}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  sx={{ flexShrink: 0 }}
                 >
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "column",
-                            gap: 4,
-                            minWidth: { xs: "100%", sm: "60%" },
-                        }}
-                    >
-                        <Box sx={{ width: { xs: "100%", sm: "60%" } }}>
-                            <Box sx={{ pb: "10px" }}>
-                                <img
-                                    style={{
-                                        objectFit: "cover",
-                                        borderRadius: "80px",
-                                    }}
-                                    width={70}
-                                    src={logo}
-                                    alt="logo of sitemark"
-                                />
-                            </Box>
-                            <Typography
-                                variant="body2"
-                                fontWeight={600}
-                                gutterBottom
-                            >
-                                Newsletter
-                            </Typography>
-                            <Typography
-                                variant="body2"
-                                color="text.secondary"
-                                mb={2}
-                            >
-                                Subscribe to our newsletter for weekly updates
-                                and promotions.
-                            </Typography>
-                            <Stack direction="row" spacing={1} useFlexGap>
-                                <TextField
-                                    id="outlined-basic"
-                                    hiddenLabel
-                                    size="small"
-                                    variant="outlined"
-                                    fullWidth
-                                    aria-label="Enter your email address"
-                                    placeholder="Your email address"
-                                    inputProps={{
-                                        autocomplete: "off",
-                                        ariaLabel: "Enter your email address",
-                                    }}
-                                />
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    sx={{ flexShrink: 0 }}
-                                >
-                                    Subscribe
-                                </Button>
-                            </Stack>
-                        </Box>
-                    </Box>
-                    <Box
-                        sx={{
-                            display: "flex",
-                            width: "100%",
-                            justifyContent: "space-between",
-                            pt: { lg: 0, md: 0, sm: "20px", xs: "20px" },
-                        }}
-                    >
-                        <Box
-                            sx={{
-                                display: { xs: "flex", sm: "flex" },
-                                flexDirection: "column",
-                                gap: 1,
-                            }}
-                        >
-                            <Typography variant="body2" fontWeight={600}>
-                                Product
-                            </Typography>
-                            <Link color="text.secondary" href="#">
-                                Features
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Testimonials
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Highlights
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Pricing
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                FAQs
-                            </Link>
-                        </Box>
-                        <Box
-                            sx={{
-                                display: { xs: "flex", sm: "flex" },
-                                flexDirection: "column",
-                                gap: 1,
-                            }}
-                        >
-                            <Typography variant="body2" fontWeight={600}>
-                                Company
-                            </Typography>
-                            <Link color="text.secondary" href="#">
-                                About us
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Careers
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Press
-                            </Link>
-                        </Box>
-                        <Box
-                            sx={{
-                                display: { xs: "flex", sm: "flex" },
-                                flexDirection: "column",
-                                gap: 1,
-                            }}
-                        >
-                            <Typography variant="body2" fontWeight={600}>
-                                Legal
-                            </Typography>
-                            <Link color="text.secondary" href="#">
-                                Terms
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Privacy
-                            </Link>
-                            <Link color="text.secondary" href="#">
-                                Contact
-                            </Link>
-                        </Box>
-                    </Box>
-                </Box>
-            </Container>
-        </div>
-    );
+                  Subscribe
+                </Button>
+              </Stack>
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "space-between",
+              pt: { lg: 0, md: 0, sm: "20px", xs: "20px" },
+            }}
+          >
+            <Box
+              sx={{
+                display: { xs: "flex", sm: "flex" },
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
+              <Typography variant="body2" fontWeight={600}>
+                Product
+              </Typography>
+              <Link color="text.secondary" href="#">
+                Features
+              </Link>
+              <Link color="text.secondary" href="#">
+                Testimonials
+              </Link>
+              <Link color="text.secondary" href="#">
+                Highlights
+              </Link>
+              <Link color="text.secondary" href="#">
+                Pricing
+              </Link>
+              <Link color="text.secondary" href="#">
+                FAQs
+              </Link>
+            </Box>
+            <Box
+              sx={{
+                display: { xs: "flex", sm: "flex" },
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
+              <Typography variant="body2" fontWeight={600}>
+                Company
+              </Typography>
+              <Link color="text.secondary" href="#">
+                About us
+              </Link>
+              <Link color="text.secondary" href="#">
+                Careers
+              </Link>
+              <Link color="text.secondary" href="#">
+                Press
+              </Link>
+            </Box>
+            <Box
+              sx={{
+                display: { xs: "flex", sm: "flex" },
+                flexDirection: "column",
+                gap: 1,
+              }}
+            >
+              <Typography variant="body2" fontWeight={600}>
+                Legal
+              </Typography>
+              <Link color="text.secondary" href="#">
+                Terms
+              </Link>
+              <Link color="text.secondary" href="#">
+                Privacy
+              </Link>
+              <Link color="text.secondary" href="#">
+                Contact
+              </Link>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </div>
+  );
 };
